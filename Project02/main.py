9262d03e02400da61a6055b3c5b7b5733fb20d79
HuyVu12 <64604579+HuyVu12@users.noreply.github.com>
2025-03-11 02:11:10 +0700

    init

---
+++ Project02/main.py
@@ -0,0 +1,80 @@
+import streamlit as st
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+from my_model import calc_model, predict
+
+st.set_page_config(page_icon='😶', page_title='Nothing', layout='wide')
+
+session = st.session_state
+if 'x' not in session: session['x'] = np.array([1, 1, 2, 4, 6])
+if 'y' not in session: session['y'] = np.array([1, 2, 4, 2, 7])
+
+st.header("ML Regression 😶")
+
+plt.figure()
+plt.plot(session['x'], session['y'], '.')
+
+df = pd.DataFrame()
+df['x'] = session['x']
+df['y'] = session['y']
+col1, col2, col3 = st.columns((1, 1, 1))
+with col1:
+    st.pyplot(plt)
+with col2:
+    with st.expander('Data information'):
+        st.dataframe(df)
+        with st.form('Form Data'):
+            x_input = st.number_input('x value')
+            y_input = st.number_input('y value')
+
+            submit_btn = st.form_submit_button()
+        if submit_btn:
+            session['x'] = np.append(session['x'], [x_input])
+            session['y'] = np.append(session['y'], [y_input])
+            st.rerun()
+with col3:
+    with st.expander('Model information'):
+        if 'model_arg' not in session: session['model_arg'] = ''
+        model_arg = st.text_input('Edit Hyperparameter', value=session['model_arg'])
+        if model_arg != session['model_arg']:  
+            session['model_arg'] = model_arg  
+            st.rerun()  
+        if 'str_latex' not in session: session['str_latex'] = ''
+        if 'str_latex_model' not in session: session['str_latex_model'] = ''
+        val = session['model_arg'].split()
+        session['str_latex'] = ''
+        for i in range(len(val)):
+            session['str_latex'] += f'\Phi_{i}x^{{{val[i]}}}'
+            if i != len(val) - 1: session['str_latex'] += '+'
+        
+        session['str_latex'] += '=y'
+        # st.write(session['model_arg'], str_latex)
+        st.latex(
+            session['str_latex']
+        )
+        if len(session['model_arg'].split()) != 0:
+            if 'theta' not in session: session["theta"] = 0
+            session["theta"] = calc_model(session['model_arg'], session['x'], session['y'])
+            st.write(session["theta"])
+            session['str_latex_model'] = ''
+            for i in range(len(session["theta"])):
+                session['str_latex_model'] += f'{session["theta"][i]}x^{{{val[i]}}}'
+                if i != len(val) - 1: session['str_latex_model'] += '+'
+            st.latex(session['str_latex_model'])
+            plt.figure()
+            plt.plot(session['x'], session['y'], '.')
+
+            x_plot = np.linspace(min(session['x']), max(session['x']), 100)
+            y_plot = [predict(session['model_arg'], session["theta"], xi) for xi in x_plot]
+
+            mse = 0
+            for i in range(len(session['y'])):
+                yi = predict(session['model_arg'], session["theta"], session['x'][i])
+                mse += (yi - session['y'][i])**2
+            mse /= len(session['y'])
+            plt.title(f'MSE = {mse}')
+
+            plt.plot(x_plot, y_plot, 'r-')
+            st.pyplot(plt)
+
---
+++ Project02/my_model.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+def calc_model(theta_arg, x_in, y_in):
+    theta_arg = theta_arg.split()
+    m = len(x_in)
+    n = len(theta_arg)
+    x = np.zeros((m, n))
+    y = np.array(y_in).reshape((m, 1))
+    for ti in range(len(theta_arg)):
+        for i in range(len(x)):
+            x[i][ti] = x_in[i]**int(theta_arg[ti]) 
+    theta = np.linalg.pinv(x.T @ x) @ x.T @ y
+    return theta
+def predict(theta_arg, theta, x_in):
+    y = 0
+    theta_arg = theta_arg.split()
+    for i in range(len(theta)):
+        y += x_in**int(theta_arg[i]) * theta[i]
+    return y
+if __name__ == '__main__':
+    x = [1, 2, 3, 4, 5]
+    y = [1, 2, 3, 4, 5]
+    theta_arg = '0 2 3'
+    print(calc_model(theta_arg, x, y))
